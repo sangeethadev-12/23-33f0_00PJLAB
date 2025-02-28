9d22b31ca31fba32ada5f332b8cacc2e789efeeb
sangeethadev-12 <234g1a33f0@srit.ac.in>
2025-02-28 21:05:45 +0530

    Add files via upload

---
+++ week-1-1.java
@@ -0,0 +1,31 @@
+
+ class dt{
+	
+	int a;
+	byte b;
+	short c;
+	long d;
+	float e;
+	double f;
+	boolean g;
+	char h;
+  void pri(){
+
+		System.out.println("the deafault value of int:"+a);
+		System.out.println("the deafault value of byte:"+b);
+		System.out.println("the deafault value of short:"+c);
+		System.out.println("the deafault value of long:"+d);
+		System.out.println("the deafault value of float:"+e);
+		System.out.println("the deafault value of double:"+f);
+		System.out.println("the deafault value of boolean:"+g);
+		System.out.println("the deafault value of char:"+h);
+					
+                     }
+}
+public class Datatype{
+public static void main(String[] args){
+	
+dt d=new dt();
+d.pri();
+
+}}
